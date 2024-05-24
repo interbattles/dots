2aa27377fd61db0b37bd0c89304367ae9e071866
interbattles <88935519+interbattles@users.noreply.github.com>
2024-05-24 12:57:57 -0700

    removed  old stuff and fix install script

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git submodule init
+git submodule update
+
 tar --skip-old-files -xvf gnome/extensions.tar.gz -C ~/.local/share/gnome-shell
 tar --skip-old-files -xvf gnome/fonts.tar.gz -C ~/.local/share
 tar --skip-old-files -xvf gnome/icons.tar.gz -C ~/.local/share
