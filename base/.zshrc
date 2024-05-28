ff20e6b85e9c5cee30c3e32984a1ff8db8c0d56e
interbattles <88935519+interbattles@users.noreply.github.com>
2024-05-28 14:14:03 -0700

    fix themes reset my pc after gnome bug

--- base/.zshrc
+++ base/.zshrc
@@ -45,3 +45,4 @@ alias cat=bat
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+eval "$(mise activate zsh)"
--- install.sh
+++ install.sh
@@ -14,11 +14,11 @@ then
 fi
 
 echo "extracting themes"
-tar --skip-old-files -xaf gnome/extensions.tar.gz -C ~/.local/share/gnome-shell
-tar --skip-old-files -xaf gnome/fonts.tar.gz -C ~/.local/share
-tar --skip-old-files -xaf gnome/icons.tar.gz -C ~/.local/share
-tar --skip-old-files -xaf gnome/themes.tar.gz -C ~/.local/share
-tar --skip-old-files -xaf gnome/backgrounds.tar.gz -C ~/.local/share
+tar --keep-newer-files -xaf gnome/extensions.tar.gz -C ~/.local/share/gnome-shell
+tar --keep-newer-files -xaf gnome/fonts.tar.gz -C ~/.local/share
+tar --keep-newer-files -xaf gnome/icons.tar.gz -C ~/.local/share
+tar --keep-newer-files -xaf gnome/themes.tar.gz -C ~/.local/share
+tar --keep-newer-files -xaf gnome/backgrounds.tar.gz -C ~/.local/share
 
 echo "loading extensions"
 dconf load /org/gnome/shell/extensions/ < gnome/extensions.conf
@@ -27,13 +27,13 @@ dconf load /org/gnome/desktop/interface/ < gnome/interface.conf
 echo "symlinking configurations"
 stow base terminal
 
-echo "symlinking themes"
-stow -d "$HOME/.local/share/themes/Gruvbox-Dark-BL-MOD" -t "$HOME/.config" -S gtk-4.0
-
 mkdir -p ~/.themes
 mkdir -p ~/.icons
 ln -t ~/.themes/ -s ~/.local/share/themes/*
 ln -t ~/.icons/ -s ~/.local/share/icons/*
+
+echo "resetting flatpak theme"
+flatpak --user override --reset
 
 echo "applying themes to flatpak"
 flatpak --user override --filesystem="$HOME/.themes" # compatability for some gnome apps
@@ -41,8 +41,9 @@ flatpak --user override --filesystem="$HOME/.icons"
 flatpak --user override --filesystem="$HOME/.local/share/icons" # gnome seems to like this one more (vesktop) (￢_￢;)
 flatpak --user override --filesystem="$HOME/.local/share/themes"
 
-flatpak --user override --env=GTK_THEME=Gruvbox-Dark-BL-MOD # dont know if this is needed ? but thats how i got mine to work
-flatpak --user override --env=ICON_THEME=Gruvbox-plus-icon-MOD
+flatpak --user override --env=GTK_THEME="Gruvbox-Dark-BL-MOD" # dont know if this is needed ? but thats how i got mine to work
+flatpak --user override --env=ICON_THEME="Gruvbox-plus-icon-MOD"
+
 
 echo "done!!!"
 echo " > you may have to log out for some changes to apply"
