7d6446d763d414b69f0455707dae48f375aa4061
interbattles <88935519+interbattles@users.noreply.github.com>
2024-06-13 05:27:55 +0000

    neovim wip

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,19 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+vim.api.nvim_set_option("clipboard","unnamed")
+
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
