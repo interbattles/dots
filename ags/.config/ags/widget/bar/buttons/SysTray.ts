967bab0e2cb5a34ef733a68b35cf32d61867996a
interbattles <88935519+interbattles@users.noreply.github.com>
2024-07-02 15:26:52 -0700

    using dex for xdg-autostart

--- ags/.config/ags/widget/bar/buttons/SysTray.ts
+++ ags/.config/ags/widget/bar/buttons/SysTray.ts
@@ -7,33 +7,33 @@ const systemtray = await Service.import("systemtray")
 const { ignore } = options.bar.systray
 
 const SysTrayItem = (item: TrayItem) => PanelButton({
-    class_name: "tray-item",
-    child: Widget.Icon({ icon: item.bind("icon") }),
-    tooltip_markup: item.bind("tooltip_markup"),
-    setup: self => {
-        const { menu } = item
-        if (!menu)
-            return
+  class_name: "tray-item",
+  child: Widget.Icon({ icon: item.bind("icon") }),
+  tooltip_markup: item.bind("tooltip_markup"),
+  setup: self => {
+    const { menu } = item
+    if (!menu)
+      return
 
-        const id = menu.connect("popped-up", () => {
-            self.toggleClassName("active")
-            menu.connect("notify::visible", () => {
-                self.toggleClassName("active", menu.visible)
-            })
-            menu.disconnect(id!)
-        })
+    const id = menu.connect("popped-up", () => {
+      self.toggleClassName("active")
+      menu.connect("notify::visible", () => {
+        self.toggleClassName("active", menu.visible)
+      })
+      menu.disconnect(id!)
+    })
 
-        self.connect("destroy", () => menu.disconnect(id))
-    },
+    self.connect("destroy", () => menu.disconnect(id))
+  },
 
-    on_primary_click: btn => item.menu?.popup_at_widget(
-        btn, Gdk.Gravity.SOUTH, Gdk.Gravity.NORTH, null),
+  on_primary_click: btn => item.menu?.popup_at_widget(
+    btn, Gdk.Gravity.SOUTH, Gdk.Gravity.NORTH, null),
 
-    on_secondary_click: btn => item.menu?.popup_at_widget(
-        btn, Gdk.Gravity.SOUTH, Gdk.Gravity.NORTH, null),
+  on_secondary_click: btn => item.menu?.popup_at_widget(
+    btn, Gdk.Gravity.SOUTH, Gdk.Gravity.NORTH, null),
 })
 
 export default () => Widget.Box()
-    .bind("children", systemtray, "items", i => i
-        .filter(({ id }) => !ignore.value.includes(id))
-        .map(SysTrayItem))
+  .bind("children", systemtray, "items", i => i
+    .filter(({ id }) => !ignore.value.includes(id))
+    .map(SysTrayItem))
