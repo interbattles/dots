afdc621383a2bdb0caa1fc54897453595716afeb
interbattles <88935519+interbattles@users.noreply.github.com>
2024-07-14 00:08:55 -0700

    changed qt5ct

--- configs/.cache/matugen/colors.fish
+++ configs/.cache/matugen/colors.fish
@@ -1,25 +1,25 @@
-set fish_color_normal dbe5e0
-set fish_color_command fbafe3
-set fish_color_param fcafe2
+set fish_color_normal d9e6db
+set fish_color_command ffb689
+set fish_color_param ffb688
 set fish_color_redirection $fish_color_param
-set fish_color_comment ffb2bd
+set fish_color_comment fbbb49
 set fish_color_error ffb4ab
-set fish_color_escape b4c5ff
+set fish_color_escape ffade2
 set fish_color_operator $fish_color_escape
-set fish_color_end b4c5ff
-set fish_color_quote ffb2bd
-set fish_color_autosuggestion b8cbc4 --background=18211f
-set fish_color_user 87d7ab
-set fish_color_host 89d7a9
+set fish_color_end ffade2
+set fish_color_quote fbbb49
+set fish_color_autosuggestion b4ccbc --background=17221c
+set fish_color_user 74d1ff
+set fish_color_host 70d2ff
 set fish_color_valid_path --underline
-set fish_color_cwd 89d7a9
-set fish_color_cwd_root b4c5ff
-set fish_color_match --background=87d7ab
-set fish_color_search_match fcafe2 --background=18211f
-set fish_color_selection b8cbc4 --bold --background=18211f
+set fish_color_cwd 70d2ff
+set fish_color_cwd_root ffade2
+set fish_color_match --background=74d1ff
+set fish_color_search_match ffb688 --background=17221c
+set fish_color_selection b4ccbc --bold --background=17221c
 set fish_color_cancel -r
-set fish_pager_color_prefix dbe5e0 --bold --underline
-set fish_pager_color_completion b8cbc4 --background=18211f
-set fish_pager_color_description $fish_color_quote fbafe3
-set fish_pager_color_progress bfce75 --background=18211f
+set fish_pager_color_prefix d9e6db --bold --underline
+set fish_pager_color_completion b4ccbc --background=17221c
+set fish_pager_color_description $fish_color_quote ffb689
+set fish_pager_color_progress 50dbd0 --background=17221c
 set fish_color_history_current --bold
